1b42925fceb69df269bdec48637c890b13d76427
Júlia Guimarães Franco <92822185+juliaguima@users.noreply.github.com>
2022-07-18 19:24:07 -0300

    Add files via upload

---
+++ Termodinâmica/index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+       
+
+<?php
+if (isset ( $_POST['Q1']) ) {
+   
+    $Q1 = $_POST['Q1'];
+    $Q2 = $_POST['Q2'];
+    $T = $_POST['T'];
+   
+
+    if ($Q1 != "" && $Q2 != "" ) {        
+        $nq = 1-($Q2/$Q1);
+        echo "O  rendimento dessa máquina será de: ".$nq; 
+
+    } elseif ($T != "" && $Q1 != "" ) {
+       $nt = ($T/$Q1);
+       echo "O  rendimento dessa máquina será de: ".$nt; 
+
+    } elseif ($T == "" && $Q1 != ""&& $Q2 != ""  ) {
+        $nq12 = ($Q1-$Q2)/$Q1;
+        echo "O  rendimento dessa máquina será de: ".$nQ12; 
+ 
+     };
+ 
+
+}
+?>
+
+<h1>Rendimento de uma máquina térmica</h1>
+<form action="index.php" method="post">
+    <ul>
+        <li>
+        <label for="Q1">Q1</label>
+        <input type="number" name="Q1" id="Q1">
+        </li>
+        <li>
+        <label for="Q2">Q2</label>
+        <input type="number" name="Q2" id="Q2">
+        </li>
+        <li>
+        <label for="T">T</label>
+        <input type="number" name="T" id="T">
+        </li>
+        <li>
+        
+        <input id= "Cadastrar" type="submit" value="Cadastrar">
+    </ul>
+</form>
+
+
+
+</body>
+</html>
